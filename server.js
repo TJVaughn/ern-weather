89d087619c7d7e349a495f606b89710af72561f5
Trevor <hauck.trevor@gmail.com>
2019-11-27 14:54:37 -0700

    Added path node module to server as I referenced it, yet forgot to require it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const coordinates = require('./server/coordinates');
 const forecast = require('./server/forecast')
