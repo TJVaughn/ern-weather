f3385a64727f241ce671fa3b74ce9588549e61cf
Trevor <hauck.trevor@gmail.com>
2020-01-07 21:15:02 -0500

    Did some refactoring, prettified some of the UI components

--- client/src/modules/Today.js
+++ client/src/modules/Today.js
@@ -28,18 +28,6 @@ class TodayComp extends Component {
         this.setState({wind: true})
     }
 
-    // handleCircadian(){
-    //     if(currTime < today.sunsetTime){
-    //         this.timeToSunset()
-    //     }
-    //     if(currTime > today.sunriseTime){
-    //         this.timeFromSunrise()
-    //     }
-    //     if(currTime > today.sunsetTime || currTime < today.sunriseTime){
-    //         this.nightTime()
-    //     }
-    // }
-
     timeToSunset(){
         if(currTime > today.sunsetTime * 1000){
             return 0;
@@ -48,10 +36,24 @@ class TodayComp extends Component {
     }
     timeFromSunrise(){
         if(currTime < today.sunriseTime * 1000){
-            return 0;
+            return 1;
         }
         return currTime - today.sunriseTime * 1000;
     }
+    timeFromSunset(){
+        if(currTime < today.sunsetTime * 1000){
+            return 0;
+        }
+        return currTime - today.sunsetTime * 1000;
+    }
+
+    timeToSunrise(){
+        if(currTime > today.sunriseTime * 1000){
+            return 0;
+        }
+        return today.sunriseTime * 1000 - currTime;
+    }
+
     nightTime(){
         let year = new Date().getFullYear()
         let month = new Date().getMonth()
@@ -60,9 +62,8 @@ class TodayComp extends Component {
         let midnightLastNight = new Date(year, month, date, 0, 0, 0).valueOf()
 
         let timeToMidnightAfterSet = midnightTonight - today.sunsetTime * 1000;
-        console.log(timeToMidnightAfterSet)
+
         let timeFromMidnightBeforeRise = today.sunriseTime * 1000 - midnightLastNight;
-        console.log(timeFromMidnightBeforeRise)
 
         return timeToMidnightAfterSet + timeFromMidnightBeforeRise;
     }
@@ -82,6 +83,7 @@ class TodayComp extends Component {
                             {Math.round(today.temperatureHigh)}&#176;
                             <br /><span className="sub-item-desc">High</span>
                         </p>
+                    
                         <p>
                             {Math.round(curr.apparentTemperature)}&#176;
                             <br /><span className="sub-item-desc">Feels like</span>
@@ -117,6 +119,7 @@ class TodayComp extends Component {
                                     content={`Speed: \n${today.windSpeed.toFixed(0)} mph`}
                                     data={[(today.windBearing / 360 * 100) - 1, 1, 98 - (today.windBearing / 360 * 100)]}
                                     colors={['#fff0', '#12d1b8', '#fff0']}
+                                    
                                 />
                             </div>
                         </div>
@@ -132,21 +135,8 @@ class TodayComp extends Component {
                     </div>
 
                     <h3 className="align-center">Circadian</h3>
-                        
                     <div className="Weather-today-circadian">
-                    <PieChart 
-                            content={
-                            `${new Date(today.sunriseTime * 1000).toLocaleTimeString((navigator.language), {hour: '2-digit', minute: '2-digit'})} \n
-                            ${new Date(today.sunsetTime * 1000).toLocaleTimeString((navigator.language), {hour: '2-digit', minute: '2-digit'})}`
-                        }
-                        // First part is sunset, followed by blank 50, followed by sunrise
-                            data={[this.timeToSunset(), this.nightTime(), this.timeFromSunrise()]}
-                            colors={['rgb(38, 166, 240)', '#000', '#a4b']}
-                        />
-                       
-                    </div>
-                    <div className="Weather-today-circadian">
-                    <p>
+                        <p>
                             {new Date(today.sunriseTime * 1000).toLocaleTimeString((navigator.language), {hour: '2-digit', minute: '2-digit'})}
                             <br /><span className="sub-item-desc">Sunrise</span>
                         </p>
@@ -155,6 +145,37 @@ class TodayComp extends Component {
                             <br /><span className="sub-item-desc">Sunset</span>
                         </p>
                     </div>
+                    <div className="Weather-today-circadian">
+                    <PieChart 
+                            
+                        // First part is sunset, followed by blank 50, followed by sunrise
+                            data={[
+                                currTime > today.sunsetTime * 1000
+                                ? this.timeFromSunset()
+                                : 0,
+
+                                currTime > today.sunsetTime * 1000
+                                ? this.nightTime() - this.timeFromSunset()
+                                : 0,
+
+                                currTime < today.sunriseTime * 1000
+                                ? this.nightTime() - this.timeToSunrise()
+                                : 0,
+
+                                currTime < today.sunriseTime * 1000
+                                ? this.timeToSunrise()
+                                : 0,
+
+                                this.timeFromSunrise(),
+                                this.timeToSunset()
+                            ]}
+                            colors={['#000', '#000', 'rgb(38, 166, 240)', 'rgb(38, 166, 240)', 'rgb(38, 166, 240)', 'rgb(38, 166, 240)']}
+                            startAngle={0}
+                            stroke={'#12d1b8'}
+                        />
+                       
+                    </div>
+                    
 
                     <h3 className="align-center">Other:</h3>
                     <div className="Weather-today-other">
