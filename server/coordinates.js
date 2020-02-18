a1cc28a71a7aa2faf686cf60e57640def82aa99d
Trevor <hauck.trevor@gmail.com>
2020-02-18 11:50:25 -0500

    Updated yarn

--- server/coordinates.js
+++ server/coordinates.js
@@ -6,27 +6,26 @@ const coordinates = (searchText, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${key}&limit=1`
     // console.log(mapBoxUrl);
     request({url, json: true}, (error, { body }={}) => {
-                    if(error){
-                        callback(error, undefined);
-                    } else if(body.message || body.features.length < 1) {
-                        console.log(body.message)
-                        callback(body.message, undefined);
-                    } else {
-                        // console.log("Mapbox API response")
+        if(error){
+            callback(error, undefined);
+        } else if(body.message || body.features.length < 1) {
+            console.log(body.message)
+            callback(body.message, undefined);
+        } else {
+            // console.log("Mapbox API response")
 
-                        // console.log([res.body.features[0].center[1], res.body.features[0].center[0]]);
-                        const data = {
-                            lat: body.features[0].center[1],
-                            long: body.features[0].center[0],
-                            name: body.features[0].place_name
-                        }
-                        const {lat, long, name} = data;
-                        // console.log(lat, long, name)
-                        // callback(undefined, data)
-                        callback(undefined, {lat, long, name})
-                    }
-
-                })
+            // console.log([res.body.features[0].center[1], res.body.features[0].center[0]]);
+            const data = {
+                lat: body.features[0].center[1],
+                long: body.features[0].center[0],
+                name: body.features[0].place_name
+            }
+            const {lat, long, name} = data;
+            // console.log(lat, long, name)
+            // callback(undefined, data)
+            callback(undefined, {lat, long, name})
+        }
+    })
 }
 
 module.exports = coordinates
